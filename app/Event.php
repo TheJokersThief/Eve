767d3d93a90be46ab665b8c13f078d0bbd6f503a
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-01-13 18:11:17 +0000

    Create events table migration and event model
    Create a migration for events table and also an event model file. Also
    a .gitignore thing.

---
+++ app/Event.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'description', 'location_id', 'start_datetime', 'end_datetime', 'title',
+    ];
+
+    public function location(){
+    	return $this->belongsTo('App\Location');
+    }
+}
---
+++ database/migrations/2016_01_13_175326_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->integer('location_id');
+            $table->dateTime('start_datetime');
+            $table->dateTime('end_datetime');
+            $table->string('title');
+            $table->timestamps();
+
+            $table->foreign('location_id')
+                    ->references('id')
+                    ->on('locations');
+                    //onDelete('cascade')?
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
