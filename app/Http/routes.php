edc48af82c3af6d2c716b096e6720ce060ef4ce2
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-01-16 13:05:33 +0000

    Added partners page and commented out $errors
    Created a basic view for /partners, added a route in routes.php that
    returns this view, added lines to composer.json and commented out any
    references to $errors in login.blade.php - this variable has not yet
    been created.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/partners', function () {
+	return view('partners');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,31 +10,33 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {!! csrf_field() !!}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <!-- <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}"> -->
+                        <div class="form-group">
                             <label class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('email'))
+                               <!--  @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                @endif -->
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <!-- <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}"> -->
+                        <div class="form-group">
                             <label class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
                                 <input type="password" class="form-control" name="password">
 
-                                @if ($errors->has('password'))
+                               <!--  @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                                @endif -->
                             </div>
                         </div>
 
