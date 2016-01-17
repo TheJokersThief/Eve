82dda5ab694e38053d1a57904f470aed74a69746
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-01-17 16:27:23 +0000

    Updated events controller, events index and events show
    Can now view titles of all events from /events, listed in order of
    ascending start_datetime. When click on an event, takes you to a more
    detailed view of the event. Only thing is that the location is shown as
    an integer (location_id).

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
-  	<p>Index</p>
+	<ul class="collection with-header">
+	    <li class="collection-header"><h4>Events</h4></li>
+
+  	@foreach($events as $event)
+		<a href="{{ action('EventsController@show', [$event->id]) }}" class="collection-item">{{ $event->title }}</a>
+	@endforeach
+
+	</ul>
 
 @endsection
