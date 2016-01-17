3829e9820e60403696bdc0f2ff18b7eddbb57e01
Colm <me@colm.cf>
2016-01-17 17:54:50 +0000

    JS Setup for form elements @eimearc

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -2,4 +2,11 @@ $( document ).ready(function($){
 	// Materialize Setup
 	$(".button-collapse").sideNav();
 	$('.modal-trigger').leanModal();
+
+	$('.datepicker').pickadate({
+	    selectMonths: true, // Creates a dropdown to control month
+	    selectYears: 15 // Creates a dropdown of 15 years to control year
+	);
+        
+	$('select').material_select();
 });
