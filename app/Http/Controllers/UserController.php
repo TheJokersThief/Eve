c4f9b909683acd100e2c8ef81c60c74ec54afb6b
Colm Cahalane <me@colm.cf>
2016-01-18 00:57:17 +0000

    Lowdown's user create function stolen and reused
    Evan and I worked on a Laravel project called Lowdown before; you'll
    find it over @UCCNetworkingSociety. We're borrowing the user create
    function here to borrow time., but I will be reworking this later.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,62 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use Validator;
+use Redirect;
 
 class UserController extends Controller
 {
 
-    
+    /**
+     * Creates a new user.
+     * This is a generic user.store function adapted from a previous project
+     * and should definitely be brought up to date with this project /
+     * adapted so that we can use Laravel Socialite with it.
+     *
+     * 
+     * 	Data should be POSTed to this function only
+     * @return REDIRECT home
+     */
+    public function store( ){
+    	// Only allow following fields to be submitted
+        $data = Request::only( [
+                    'name',
+                    'password',
+                    'password_confirmation',
+                    'email'
+                ]);
+
+        // Validate all input
+        $validator = Validator::make( $data, [
+                    'name'  => 'required',
+                    'email'     => 'email|required|unique:users',
+                    'password'  => 'required|confirmed|min:5'
+                ]);
+
+        if( $validator->fails( ) ){
+        	// If validation fails, redirect back to 
+        	// registration form with errors
+            return Redirect::back( )
+                    ->withErrors( $validator )
+                    ->withInput( );
+        }
+
+        // Hash the password
+        $data['password'] = Hash::make($data['password']);
+        
+        // Create the new user
+        $newUser = User::create( $data );
+        if( $newUser ){
+            Auth::login($newUser);
+        	// If successful, go to home
+        	return Redirect::route( 'home' );
+        }
+        
+        // If unsuccessful, return with errors
+        return Redirect::back( )
+                    ->withErrors( [
+                    	'message' => 'We\'re sorry but registration failed, please try again later.' 
+                    ] )
+                    ->withInput( );
+    }
 }
