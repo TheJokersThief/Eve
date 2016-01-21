96d46ff650b6f798af06b7706b76b0c0a7aed08a
Colm <me@colm.cf>
2016-01-21 14:18:24 +0000

    Add QrCode

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
 
         Collective\Html\HtmlServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -207,8 +208,7 @@ return [
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Image'     => Intervention\Image\Facades\Image::class,
-
-
+        'QrCode'    => SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
     ],
 
 ];
