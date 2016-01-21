43920169d08e292965bdc891b969efd396b846fd
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-01-21 14:48:18 +0000

    updated js to format date for datepicker

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -5,7 +5,8 @@ $( document ).ready(function($){
 
 	$('.datepicker').pickadate({
 	    selectMonths: true, // Creates a dropdown to control month
-	    selectYears: 15 // Creates a dropdown of 15 years to control year
+	    selectYears: 15, // Creates a dropdown of 15 years to control year
+	    format: 'yyyy-mm-dd' // Formats the date
 	});
         
 	$('select').material_select();
