438064e632b38e387fb315465c39c770580ef78d
Evan Smith <esincork@gmail.com>
2016-01-21 14:56:31 +0000

    Move gulped JS to a different folder

--- gulpfile.js
+++ gulpfile.js
@@ -13,5 +13,5 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.less('app.less');
-    mix.scriptsIn("resources/assets/js", "public/js/main.js");
+    mix.scriptsIn("resources/assets/js", "public/js/compiled/main.js");
 });
