b5b6ffc84b542a32331e248399d93e0e194e5efe
Evan Smith <esincork@gmail.com>
2016-01-21 03:12:06 +0000

    Finished create user in install process
    Added Image Intervention to handle file uploads and do cropping, etc. It's awesome. Seriously. I love this package - it takes so much pain out of dealing with file uploads.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Validator;
+use Redirect;
+use Auth;
+use Image;
+use App\User;
+use App\Ticket;
+use App\Setting;
+use Response;
 
+use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\UserController;
-
-use Validator;
-use Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ApiController extends Controller
 {
@@ -31,13 +37,29 @@ class ApiController extends Controller
         $validator = Validator::make( $data, [
                     'name'  => 'required',
                     'email'     => 'email|required|unique:users',
-                    'password'  => 'required|confirmed|min:5'
+                    'password'  => 'required|confirmed|min:5',
+                    'profile_picture' => 'sometimes|image|max:10240' // Limit filesize to 10MB
                 ]);
 
         if( $validator->fails( ) ){
         	// If validation fails, redirect back to 
         	// registration form with errors
             return Response::json(['errors' => $validator->errors()->all()]);
+        }
+
+        // Image intervention ftw
+        if ($request->hasFile('profile_picture')){
+            $data['profile_picture'] =  UserController::uploadProfilePicture( 
+                                            $request->file('profile_picture'),
+                                            $data['email']
+                                        );
+        }
+
+        try {
+            $user = User::where('email', $data['email'] )->firstOrFail();
+            UserController::updateUser( $user->id, $data );
+        } catch (ModelNotFoundException $e) {
+            UserController::createUser( $data );
         }
     	
 	    //////////////////////////////////////////
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
-use Illuminate\Http\Request;
 use Validator;
 use Redirect;
+use Auth;
+use Image;
+use Hash;
+use Response;
+use App\User;
+use App\Ticket;
+use App\Setting;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
@@ -44,11 +53,8 @@ class UserController extends Controller
                     ->withInput( );
         }
 
-        // Hash the password
-        $data['password'] = Hash::make($data['password']);
-        
-        // Create the new user
-        $newUser = User::create( $data );
+        $newUser = createUser( $data );
+
         if( $newUser ){
             Auth::login($newUser);
         	// If successful, go to home
@@ -61,5 +67,66 @@ class UserController extends Controller
                     	'message' => 'We\'re sorry but registration failed, please try again later.' 
                     ] )
                     ->withInput( );
+    }
+
+    /**
+     * Creates a new user
+     * @param  array  $userInfo User's data
+     * @return User             The newly created User object
+     */
+    public static function createUser( $userInfo ){
+        // Hash the password
+        $userInfo['password'] = Hash::make($userInfo['password']);
+        
+        // Create the new user
+        $newUser = User::create( $userInfo );
+
+        return $newUser;
+    }
+
+    /**
+     * Updates a user's account info
+     * @param  int    $userID   The ID of the user we want to update
+     * @param  array  $userInfo The data to be added/updated
+     * @return User             User object we just updated
+     */
+    public static function updateUser( $userID, $userInfo ){
+        if( isset( $userInfo['password'] ) ){
+            // If the password's being updated, we need to hash it
+            $userInfo['password'] = Hash::make( $userInfo['password'] );
+        }
+
+        $user = User::find( $userID );
+        foreach ($userInfo as $key => $value) {
+            $user->$key = $value;
+        }
+        $user->save();
+        return $user;
+    }
+
+    /**
+     * Uploads and crops user profile picture
+     * @param  File    $image   File from form request
+     * @return string           URL-friendly path to image
+     */
+    public static function uploadProfilePicture( $image, $email ){
+        // Get the file from the request
+        $file = $image;
+
+        $destination_path = storage_path() .'/uploads/';
+        // Create a filename by hashing the user's username. This
+        // will mean each user only has one profile picture residing
+        // on our filesystem
+        $file_name = hash('ripemd160', $email ) .'_picture.'. $file->getClientOriginalExtension();
+        // Move the file to our server
+        $movement = $image->move($destination_path, $file_name);
+
+        // Perform an image intervention, getting best fit from image
+        // and saving it again
+        $image = Image::make( storage_path(). '/uploads/'. $file_name);
+        $image->fit( 500, 500 );
+        $image->save(storage_path(). '/uploads/'. $file_name);
+
+        return (string) "/images/". $file_name;
     }
 }
