e99af78885f525608eb13fc6d6f22db47620ff2f
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-01-23 17:18:40 +0000

    updated show view for individual event

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="container">
         <div class="col s12">
-        	<h1>{{ $event->title }}</h1>
+        	<h2>{{ $event->title }}</h2>
         </div>
 		<div class="divider"></div>
   			<div class="section">
@@ -13,15 +13,43 @@
       	<div class="divider"></div>
   			<div class="section">
   			<div class="row">
-  			<div class="col s6">Start:</div>
-      		<div class="col s6">End:</div>
-		    <div class="col s6">{{ $event->start_datetime }}</div>
-      		<div class="col s6">{{ $event->end_datetime }}</div>
+	  			<div class="col s6"><h5>Start:</h5></div>
+	      		<div class="col s6"><h5>End:</h5></div>
+			    <div class="col s6">{{ $event->start_datetime }}</div>
+	      		<div class="col s6">{{ $event->end_datetime }}</div>
 	      	</div>
 		</div>
 		<div class="divider"></div>
   			<div class="section">
-		    <p>{{ $event->location_id }}</p>
+  			<div class="row">
+  				<div class="col s2"><h5>Location:<h5></div>
+			    <div class="col s10"><p>{{ $location_name }}</p></div>
+			</div>
+		</div>
+		<div class="divider"></div>
+  			<div class="section">
+			    <h5>Event Partners</h5>
+			    <div class="row">
+				    @foreach($partners as $partner)
+				    	<div class="col s4">
+							<div class="card small">
+					            <div class="card-image">
+					              	<img src="{{ URL::to('/') }}/{{$partner->media->file_location}}">
+					              	<span class="card-title">{{$partner->name}}</span>
+					            </div>
+					            <div class="card-content">
+					              	<p>{{$partner->description}}</p>
+					              	<p>Price: {{$partner->price}}</p>
+					              	<p>Distance: {{$partner->distance}}</p>
+					            </div>
+					            <div class="card-action">
+					              	<a href="/partners/{{$partner->id}} ">more info</a>
+					            </div>
+				            </div>
+			          	</div>
+					@endforeach
+				</div>
+			</div>
 		</div>
 	</div>
 
