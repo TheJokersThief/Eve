ca85ce6b3fe55959d3a434b25b271559b22d27c0
Evan Smith <esincork@gmail.com>
2016-01-23 00:49:39 +0000

    Make the login modal smaller because OCD

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,11 @@
-
-	<div id="login-modal" class="modal">
-	    <div class="modal-content">
-	    	<h3> Login</h3>
-	      	@include('forms.login')
-	    </div>
-	    <div class="modal-footer">
-	      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">&times;</a>
-	    </div>
+	<div class="row">
+		<div id="login-modal" class="col s12 m4 offset-m4 modal">
+		    <div class="modal-content">
+		    	<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat right">&times;</a>
+		    	<h3> Login</h3>
+		      	@include('forms.login')
+		    </div>
+		</div>
 	</div>
 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
