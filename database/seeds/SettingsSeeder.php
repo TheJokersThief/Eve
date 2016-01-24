11018c0dffc060d89cb589f0dbcdedf8a6967b9c
Evan Smith <esincork@gmail.com>
2016-01-24 19:51:53 +0000

    Add default company_logo

--- database/seeds/SettingsSeeder.php
+++ database/seeds/SettingsSeeder.php
@@ -14,9 +14,9 @@ class SettingsSeeder extends Seeder
 
 		Setting::firstOrCreate(['name' => 'is_installed', 'setting' => 'no']);
 		Setting::firstOrCreate(['name' => 'default_profile_picture', 'setting' => '/images/default_profile_image.png' ] );
-		Setting::firstOrCreate(['name' => 'company_name', 'setting' => 'Project Eve' ] );
+		Setting::firstOrCreate(['name' => 'company_name', 'setting' => 'Eve Online' ] );
 		Setting::firstOrCreate(['name' => 'description', 'setting' => 'We are an awesome company' ] );
-		Setting::firstOrCreate(['name' => 'company_logo', 'setting' => '/images/default_profile_image.png' ] );
-		Setting::firstOrCreate(['name' => 'company_logo_white', 'setting' => '/images/default_profile_image.png' ] );
+		Setting::firstOrCreate(['name' => 'company_logo', 'setting' => '/images/logo_full.png' ] );
+		Setting::firstOrCreate(['name' => 'company_logo_white', 'setting' => '/images/logo_full_white.png' ] );
 	}
 }
