5c5939053952b3efd654b3005119989b13468e9f
Evan Smith <esincork@gmail.com>
2016-01-24 02:42:43 +0000

    Seed the news

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call(LocationsTableSeeder::class);
         $this->call(EventPartnersTableSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(NewsSeeder::class);
 
         Model::reguard();
     }
