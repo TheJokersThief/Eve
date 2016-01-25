fb7612fea9cbf93e57d338becd49ee703b2b5bfb
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-01-25 08:44:53 +0000

    Added button functionality for creating new event
    Looks horrible, but it works.

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,13 +2,25 @@
 
 @section('content')
 
-	<ul class="collection with-header">
-	    <li class="collection-header"><h4>Events</h4></li>
+	<div class="container">
 
-  	@foreach($events as $event)
-		<a href="{{ action('EventsController@show', [$event->id]) }}" class="collection-item">{{ $event->title }}</a>
-	@endforeach
+		<ul class="collection with-header">
+			<div class="row">
+			    <div class="col s11">
+			    	<li class="collection-header"><h4>Events</h4></li>
+			    </div>
+				<div class="col s1"><a href="{{ URL::route('events.create') }}"class="btn-floating btn-large waves-effect waves-light red">
+					<h5 class="material-icons">+</h5></a>
+				</div>
+			</div>
+			<div class="row">
+	  	@foreach($events as $event)
+			<a href="{{ action('EventsController@show', [$event->id]) }}" class="collection-item">{{ $event->title }}</a>
+		@endforeach
+			</div>
 
-	</ul>
+		</ul>
+
+	</div>
 
 @endsection
