9b0e683a6696144a8a507780a9efce549df9ff12
darr758 <darraghk500@yahoo.ie>
2016-02-04 23:55:25 +0000

    Created migration for new columns in User model

---
+++ database/migrations/2016_02_03_200002_addCountryCityToUser.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCountryCityToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('country');
+            $table->string('city');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
