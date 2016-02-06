47a070b4ad3bbae667c8946aa409a0cfae8895d9
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-02-06 12:19:33 +0000

    events index looks better now

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-	<div class="container">
+	<!-- <div class="container">
 
 		<ul class="collection with-header">
 			<div class="row">
@@ -21,6 +21,30 @@
 
 		</ul>
 
-	</div>
+	</div> -->
+
+	<ul class="collection with-header">
+	    <li class="collection-header"><h4>events</h4></li>
+	    <div class="row">
+		    @foreach($events as $event)
+		    	<div class="col s4">
+		            <div class="card">
+		            	<div class="card-image waves-effect waves-block waves-light">
+					    	<img class="activator" src="{{$event->featured_image}}">
+					    </div>
+					    <div class="card-content">
+					    	<span class="card-title activator grey-text text-darken-4">{{$event->title}}<i class="material-icons right">more</i></span>
+					      	<p><a href="{{action('EventsController@show', [$event->id])}}">visit page</a></p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4">{{$event->title}}<i class="material-icons right">close</i></span>
+					      <p><a href="{{action('EventsController@show', [$event->id])}}">visit page</a></p>
+					      <p>{!! $event->description !!}</p>
+					    </div>
+					</div>
+	          	</div>
+			@endforeach
+		</div>
+	</ul>
 
 @endsection
