30979a0d0e10cec33eebaaee58540b330a6ead47
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-02-07 18:34:22 +0000

    updated partners page to look not nausea inducing

--- resources/views/partners/show.blade.php
+++ resources/views/partners/show.blade.php
@@ -6,22 +6,34 @@
         <div class="col s12">
         	<h1>{{ $partner->name }}</h1>
         </div>
+        <div class="parallax-container">
+			<div class="parallax">
+				<img src="{{ $partner->media->file_location }}">
+			</div>
+		</div>
 		<div class="divider"></div>
   			<div class="section">
-		    <p>{{ $partner->description }}</p>
+		    <p>{!! $partner->description !!}</p>
 		</div>
       	<div class="divider"></div>
-  			<div class="section">
-  			<div class="row">
-		    <div class="col s3">Type: {{ $partner->type }}</div>
-      		<div class="col s3">Price: {{ $partner->price }}</div>
-      		<div class="col s3">Distance: {{ $partner->distance }}</div>
-      		<div class="col s3">Email: {{ $partner->email }}</div>
+		<div class="section">
+			<div class="row">
+				<div class="col s6"><h5>Type:</h5></div>
+				<div class="col s6"><h5>Price:</h5></div>
+			    <div class="col s6">{{ $partner->type }}</div>
+		  		<div class="col s6">{{ $partner->price }}</div>
+	  		</div>
+	  		<div class="row">
+		  		<div class="divider"></div>
+		  		<div class="col s6"><h5>Distance:</h5></div>
+				<div class="col s6"><h5>Email:</h5></div>
+		  		<div class="col s6">{{ $partner->distance }}</div>
+		  		<div class="col s6">{{ $partner->email }}</div>
 	      	</div>
-		</div>
-		<div class="divider"></div>
-  			<div class="section">
-		    <p>Location id: {{ $partner->location_id }}</p>
+		      	<div class="divider"></div>
+		  		<div class="col s2"><h5>Location:<h5></div>
+		    	<div class="col s10">{{ $partner->location->name }}</div>
+			</div>
 		</div>
 	</div>
 
