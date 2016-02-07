3198bd923100da975613436a170a23588c737835
Colm Cahalane <me@colm.cf>
2016-02-07 12:38:28 +0000

    fixed dangling div

--- resources/views/events/details.blade.php
+++ resources/views/events/details.blade.php
@@ -26,27 +26,27 @@
 	</div>
 </div>
 <div class="divider"></div>
-	<div class="section">
-	    <h5>Event Partners</h5>
-	    <div class="row">
-		    @foreach($event->partners as $partner)
-		    	<div class="col s4">
-            <div class="card">
-			    <div class="card-image waves-effect waves-block waves-light">
-			    	<img class="activator" src="{{ URL::to('/') }}/{{$partner->media->file_location}}">
-			    </div>
-			    <div class="card-content">
-			    	<span class="card-title activator grey-text text-darken-4">{{$partner->name}}<i class="material-icons right">more</i></span>
-			      	<p><a href="{{action('PartnersController@show', [$partner->id])}}">visit page</a></p>
-			    </div>
-			    <div class="card-reveal">
-			      <span class="card-title grey-text text-darken-4">{{$partner->name}}<i class="material-icons right">close</i></span>
-			      <p><a href="{{action('PartnersController@show', [$partner->id])}}">visit page</a></p>
-			      <p>{{$partner->description}}</p>
-			    </div>
-			</div>
-      	</div>
-			@endforeach
-		</div>
-	</div>
+<div class="section">
+    <h5>Event Partners</h5>
+    <div class="row">
+        @foreach($event->partners as $partner)
+            <div class="col s4">
+        <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="{{ URL::to('/') }}/{{$partner->media->file_location}}">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">{{$partner->name}}<i class="material-icons right">more</i></span>
+                <p><a href="{{action('PartnersController@show', [$partner->id])}}">visit page</a></p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">{{$partner->name}}<i class="material-icons right">close</i></span>
+              <p><a href="{{action('PartnersController@show', [$partner->id])}}">visit page</a></p>
+              <p>{{$partner->description}}</p>
+            </div>
+        </div>
+    </div>
+        @endforeach
+    </div>
 </div>
+
