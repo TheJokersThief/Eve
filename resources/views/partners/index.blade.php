a55a8f45ae4ce8f70578ef3d527d0e2525c1de77
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-02-08 11:46:33 +0000

    make partners index look better

--- resources/views/partners/index.blade.php
+++ resources/views/partners/index.blade.php
@@ -2,29 +2,29 @@
 
 @section('content')
 
-	<ul class="collection with-header">
-	    <li class="collection-header"><h4>Partners</h4></li>
-	    <div class="row">
-		    @foreach($partners as $partner)
-		    	<div class="col s4">
-		            <div class="card">
-					    <div class="card-image waves-effect waves-block waves-light">
-					    	<img class="activator" src="{{ URL::to('/') }}/{{$partner->media->file_location}}">
-					    </div>
-					    <div class="card-content">
-					    	<span class="card-title activator grey-text text-darken-4">{{$partner->name}}<i class="material-icons right">more</i></span>
-					      	<p><a href="{{action('PartnersController@show', [$partner->id])}}">visit page</a></p>
-					    </div>
-					    <div class="card-reveal">
-					      <span class="card-title grey-text text-darken-4">{{$partner->name}}<i class="material-icons right">close</i></span>
-					      <p><a href="{{action('PartnersController@show', [$partner->id])}}">visit page</a></p>
-					      <p>{{$partner->description}}</p>
-					    </div>
-					</div>
-	          	</div>
-			@endforeach
-		</div>
-	</ul>
+<div class="container">
+    <div class="col s12 m12 center-align"><h4>Partners</h4></div>
+    <div class="row">
+	    @foreach($partners as $partner)
+	    	<div class="col s4">
+	            <div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				    	<img class="activator" src="{{ URL::to('/') }}/{{$partner->media->file_location}}">
+				    </div>
+				    <div class="card-content">
+				    	<span class="card-title activator grey-text text-darken-4">{{$partner->name}}<i class="material-icons right">more</i></span>
+				      	<p><a href="{{action('PartnersController@show', [$partner->id])}}">visit page</a></p>
+				    </div>
+				    <div class="card-reveal">
+				      <span class="card-title grey-text text-darken-4">{{$partner->name}}<i class="material-icons right">close</i></span>
+				      <p><a href="{{action('PartnersController@show', [$partner->id])}}">visit page</a></p>
+				      <p>{{$partner->description}}</p>
+				    </div>
+				</div>
+          	</div>
+		@endforeach
+	</div>
+</div>
 
 @endsection
 
