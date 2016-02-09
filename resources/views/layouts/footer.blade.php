21e1d5b51805f19652f7e2a6874cb35f872dac2f
Colm Cahalane <me@colm.cf>
2016-02-09 21:13:23 +0000

    this too is just something that should be done...

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,14 @@
-	<div class="row modal-row">
-		<div id="login-modal" class="col s12 m4 offset-m4 modal">
-		    <div class="modal-content">
-		    	<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat right">&times;</a>
-		    	<h3> Login</h3>
-		      	@include('forms.login')
-		    </div>
+	@if(!Auth::check())
+		<div class="row modal-row">
+			<div id="login-modal" class="col s12 m4 offset-m4 modal">
+			    <div class="modal-content">
+			        <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat right">&times;</a>
+			        <h3> Login</h3>
+			        @include('forms.login')
+			    </div>
+			</div>
 		</div>
-	</div>
+	@endif
 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
