a04bb87ec298721a83cdb36390aca18f07d8e316
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-02-10 19:15:03 +0000

    fixed #11
    added button for creating event from events index

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -27,27 +27,33 @@
 	</div> -->
 
 <div class="container">
-	<div class="col s12 m12 center-align"><h4>Events</h4></div>
-		<div class="row">
-		    @foreach($events as $event)
-		    	<div class="col s4">
-		            <div class="card">
-		            	<div class="card-image waves-effect waves-block waves-light">
-					    	<img class="activator" src="{{$event->featured_image}}">
-					    </div>
-					    <div class="card-content">
-					    	<span class="card-title activator grey-text text-darken-4">{{$event->title}}<i class="material-icons right">more</i></span>
-					      	<p><a href="{{action('EventsController@show', [$event->id])}}">visit page</a></p>
-					    </div>
-					    <div class="card-reveal">
-					      <span class="card-title grey-text text-darken-4">{{$event->title}}<i class="material-icons right">close</i></span>
-					      <p><a href="{{action('EventsController@show', [$event->id])}}">visit page</a></p>
-					      <p>{!! $event->description !!}</p>
-					    </div>
-					</div>
-		      	</div>
-			@endforeach
+	<div class="row">
+		<div class="valign-wrapper">
+			<div class="col s10 m10">
+				<h4>Events</h4>
+			</div>
+			<div class="col s2 m2">
+				<a href="{{ action('EventsController@create') }}" class="waves-effect waves-light btn">Create</a>
+			</div>
 		</div>
+	    @foreach($events as $event)
+	    	<div class="col s4">
+	            <div class="card">
+	            	<div class="card-image waves-effect waves-block waves-light">
+				    	<img class="activator" src="{{$event->featured_image}}">
+				    </div>
+				    <div class="card-content">
+				    	<span class="card-title activator grey-text text-darken-4">{{$event->title}}<i class="material-icons right">more</i></span>
+				      	<p><a href="{{action('EventsController@show', [$event->id])}}">visit page</a></p>
+				    </div>
+				    <div class="card-reveal">
+				      <span class="card-title grey-text text-darken-4">{{$event->title}}<i class="material-icons right">close</i></span>
+				      <p><a href="{{action('EventsController@show', [$event->id])}}">visit page</a></p>
+				      <p>{!! $event->description !!}</p>
+				    </div>
+				</div>
+	      	</div>
+		@endforeach
 	</div>
 </div>
 
