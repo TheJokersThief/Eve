5cc70aa011eb8bc78dcc6c6b10fd28c01187210a
Evan Smith <esincork@gmail.com>
2016-02-11 12:12:56 +0000

    New user cards

--- resources/views/user/pastEvents.blade.php
+++ resources/views/user/pastEvents.blade.php
@@ -15,24 +15,26 @@
 	  		</nav>
 		<div class="col m10 s12 push-m1">
 			<div class="col m3">
-				<div class="hide-on-small-only" id="userInfo">
-					<div class="collection">
-						<div class="hide-on-med-and-down">
-							<img src="{{$me->profile_picture}}">
-						</div>
-						<div class="row">
-							<div class="col s10">
-								<span>User Name: {{$me->name}}</span>
-								<p>{{$me->bio}}</p>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col s12">
-								<a href="{{ URL::route('user/edit', Crypt::encrypt($me->id)) }}"class="waves-effect waves-light btn">Edit profile</a>
-							</div>
-						</div>
-					</div>
-				</div>
+				<div id="profile-card" class="card hide-on-small-only">
+	                <div class="card-image waves-effect waves-block waves-light">
+	                    <img class="activator" src="{{ URL::to('/') . '/images/red-geometric-background.png'}}" alt="user background">
+	                </div>
+	                <div class="card-content">
+	                    <img src="{{$me->profile_picture}}" alt="" class="circle responsive-img activator card-profile-image">
+	                    <a href="{{ URL::route('user/edit', Crypt::encrypt($me->id)) }}" class="btn-floating btn-move-up waves-effect waves-light darken-2 right">
+	                        <i class="mdi-action-account-circle"></i>
+	                    </a>
+
+	                    <span class="card-title activator grey-text text-darken-4">{{ $me->name }}</span>
+	                    <p><i class="mdi-communication-email cyan-text text-darken-2"></i> {{ $me->email }}</p>
+	                    <p><i class="fa fa-map-marker cyan-text text-darken-2"></i> {{ $me->city }}, {{ $me->country }}</p>
+	                    <p><i class="fa fa-language cyan-text text-darken-2"></i> {{ $me->language }}</p>
+	                </div>
+	                <div class="card-reveal">
+		                    <span class="card-title grey-text text-darken-4">Roger Waters <i class="mdi-navigation-close right"></i></span>
+		                    <p>{{ str_limit( $me->bio, 300 ) }}</p>
+		                </div>
+	            </div>
 			</div>
 				<div id="upComingEvents" class="col m9 s12">
 					<div class="">
