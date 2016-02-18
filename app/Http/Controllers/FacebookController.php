7d3077e0514d3d8a893caaf1beb142bfa23a84cb
Colm Cahalane <me@colm.cf>
2016-02-18 01:08:58 +0000

    doc

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -18,18 +18,21 @@ class FacebookController extends Controller
 	 * Callback to log in users from Facebook. Be warned; it's messy.
 	 */
 	public function authenticateFromJavascript(){
+
+		// Try to grab the Facebook API token that the SDK should give us
 		try {
 			$token = Facebook::getJavaScriptHelper()->getAccessToken();
 		} catch (Facebook\Exceptions\FacebookSDKException $e) {
-			// Failed to obtain access token
+			// Failed to obtain access token; error out
 			return Redirect::to('/login')->withErrors([$e->getMessage()]);
 		}
 		if(!$token){
+			// If the token is falsy... uhh, what the hell
 			return Redirect::to('/login')->withErrors(["You haven't logged into Facebook correctly."]);
 		}
 
 		if (! $token->isLongLived()) {
-			// OAuth 2.0 client handler
+			// OAuth 2.0 client handler. This is... a thing.
 			$oauth_client = Facebook::getOAuth2Client();
 
 			// Extend the access token.
@@ -40,17 +43,23 @@ class FacebookController extends Controller
 			}
 		}
 
+		// Keep the User Access Token in Session storage, so we can use it for this request
 		Session::put('fb_user_access_token', (string) $token);
+
+		// Use the user's access token by default for this request
 		Facebook::setDefaultAccessToken($token);
 
 		try {
+			// Request Facebook user data
 			$response = Facebook::get('/me?fields=id,name,email,location,bio,picture.width(800).height(800)');
 		} catch (Facebook\Exceptions\FacebookSDKException $e) {
-			dd($e->getMessage());
+			Redirect::back()->withErrors([$e->getMessage()]);
 		}
 
+		// get a Graph User object
 		$facebook_user = $response->getGraphUser();
 
+		// Use the Graph User to create a Laravel User with their data
 		$user = User::createOrUpdateGraphNode($facebook_user);
 
 		// Log the user into Laravel
