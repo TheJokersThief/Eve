5feabe40030fe14417e809cd721d51aab44ebf40
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-02-20 19:23:04 +0000

    changed location seeder (forgot minus for longitude)

--- database/seeds/LocationSeeder.php
+++ database/seeds/LocationSeeder.php
@@ -14,36 +14,36 @@ class LocationSeeder extends Seeder
     public function run(){
     	Location::firstOrCreate([
     		'name' => "1 Yellow Brick Road, Cork City, Cork",
-    		'latitude' => 53.347503,
-            'longitude' => 6.228647,
+    		'latitude' => 51.898202,
+            'longitude' => -8.479321,
     		'capacity' => "200",
     		'featured_image' => '/images/sample_images/venues/1.jpg'
     	]);
     	Location::firstOrCreate([
     		'name' => "2 Yellow Brick Road, Cork City, Cork",
-    		'latitude' => 53.347503,
-            'longitude' => 6.228647,
+    		'latitude' => 51.898202,
+            'longitude' => -8.479321,
     		'capacity' => "200",
     		'featured_image' => '/images/sample_images/venues/1.jpg'
     	]);
     	Location::firstOrCreate([
     		'name' => "3 Yellow Brick Road, Cork City, Cork",
-    		'latitude' => 53.347503,
-            'longitude' => 6.228647,
+    		'latitude' => 51.898202,
+            'longitude' => -8.479321,
     		'capacity' => "200",
     		'featured_image' => '/images/sample_images/venues/1.jpg'
     	]);
     	Location::firstOrCreate([
     		'name' => "4 Yellow Brick Road, Cork City, Cork",
-    		'latitude' => 53.347503,
-            'longitude' => 6.228647,
+    		'latitude' => 51.898202,
+            'longitude' => -8.479321,
     		'capacity' => "200",
     		'featured_image' => '/images/sample_images/venues/1.jpg'
     	]);
     	Location::firstOrCreate([
     		'name' => "5 Yellow Brick Road, Cork City, Cork",
-    		'latitude' => 53.347503,
-            'longitude' => 6.228647,
+    		'latitude' => 51.898202,
+            'longitude' => -8.479321,
     		'capacity' => "200",
     		'featured_image' => '/images/sample_images/venues/1.jpg'
     	]);
