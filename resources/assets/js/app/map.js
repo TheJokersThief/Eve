c779349b1c07dc9cc2c3ec99467fb3936b785528
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-02-20 11:44:13 +0000

    centre and zoom map on ireland

--- resources/assets/js/app/map.js
+++ resources/assets/js/app/map.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 3,
+    center: {lat: 53.402119, lng: -7.724075},
+    zoom: 6,
     styles: [{
       featureType: 'poi',
       stylers: [{ visibility: 'off' }]  // Turn off points of interest.
