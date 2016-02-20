aa4c7b40c2cb3a4a708c7dbcefc027c6a0e7028a
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2016-02-20 12:51:46 +0000

    added some css to the info pack view

--- resources/views/events/infopack.blade.php
+++ resources/views/events/infopack.blade.php
@@ -15,20 +15,33 @@
 
 <div class="container">
 	<div class="row">
+		<div class="card">
+			<div class="card-header red lighten-2">
+				<div class="card-title" style="padding: .1%;">
+					<p style="margin-left: 5%;">{!! $ticket->event->title !!}</p>
+				</div>
+			</div>
+			<div class="card-content">
+				{!! $ticket->event->description !!}
+			</div>
+		</div>
+		<div class="col s12 m4 l4">
 			<div class="card">
-				<div class="card-header red lighten-2">
+				<div class="card-header blue">
 					<div class="card-title">
-						<h4 class="">{!! $ticket->event->title !!}</h4>
+						<p class="center-align">Your Ticket</p>
 					</div>
 				</div>
 				<div class="card-content">
-					{!! $ticket->event->description !!}
+					<div class="row">
+						<div class="col s12 center-align">
+							{!! $ticket->qr() !!}
+						</div>
+					</div>
 				</div>
 			</div>
-		<div class="col s12 center-align">
-			{!! $ticket->qr() !!}
 		</div>
-		<div id="map" class="col s12 center-align" style="height: 400px;"></div>
+		<div id="map" class="col s12 center-align" style="width: 65%; height: 400px;"></div>
 		@foreach($ticket->event->partners as $partner)
 		<div class="col s4">
 	      <div class="card blue-grey darken-1">
