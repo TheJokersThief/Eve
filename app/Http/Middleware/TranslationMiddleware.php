8878225d754c30295b5777fa5b66c461ece9c26f
Evan Smith <esincork@gmail.com>
2016-02-25 01:26:32 +0000

    Refine translation middleware

--- app/Http/Middleware/TranslationMiddleware.php
+++ app/Http/Middleware/TranslationMiddleware.php
@@ -9,15 +9,29 @@ use View;
 
 class TranslationMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next( $request )->withCookie(cookie()->forever('locale', 'fr'));
-    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+
+		if( Auth::check() ){
+			// If logged in, take the user's language setting
+			$language = Auth::user()->language;
+		} else {
+			// If not logged in, rely on cookies
+			if( $request->hasCookie( 'locale' ) ){
+				// If cookie exists, use it
+				$language = $request->cookie('locale');
+			} else{
+				// Otherwise, English is our default language
+				$language = 'en';
+			}
+		}
+		return $next( $request )->withCookie(cookie()->forever('locale', $language));
+	}
 }
