190590bc993e1fafeaaf58b32e4637bc2e981b54
Evan Smith <esincork@gmail.com>
2016-03-03 11:56:45 +0000

    Missed a div

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -100,6 +100,7 @@
 					</div>
 				</div>
 			</div>
+		</div>
 		@endif
 
 		<section class="event-image valign-wrapper" style="background-image: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('{{ $event->featured_image }}');;">
